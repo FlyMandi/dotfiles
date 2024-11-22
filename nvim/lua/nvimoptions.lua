7dab4c347fe5667f64add71a81df1d548ee0d382
Mandi <115730623+FlyMandi@users.noreply.github.com>
2024-11-22 01:04:02 +0100

    clean nvimoptions.lua

--- nvim/lua/nvimoptions.lua
+++ nvim/lua/nvimoptions.lua
@@ -1,15 +1,4 @@
--- set line numbering and no wrap
-vim.wo.number = true
-vim.wo.relativenumber = true
-
--- format options from https://neovim.io/doc/user/change.html#fo-table
--- TODO: find a way to set these options after \ftplugin\lua.lua, possible on windows?
-vim.opt.formatoptions:remove("c")
-vim.opt.formatoptions:remove("r")
-vim.opt.formatoptions:remove("o")
-vim.opt.formatoptions:remove("t")
-
--- set from tutorial
+-- set basic options
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
@@ -18,3 +7,6 @@ vim.cmd("set shiftwidth=2")
 -- set vim to yank to system clipboard
 vim.opt.clipboard = 'unnamedplus'
 
+-- set relative line numbering
+vim.wo.number = true
+vim.wo.relativenumber = true
