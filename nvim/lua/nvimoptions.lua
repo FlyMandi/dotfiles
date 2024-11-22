3e9554535aae99e423893cc77e0f28d67c62be6f
Federico <steidlmartinez@gmail.com>
2024-11-22 10:40:20 +0100

    increase tab width

--- nvim/lua/nvimoptions.lua
+++ nvim/lua/nvimoptions.lua
@@ -1,8 +1,8 @@
 -- set basic options
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 
 -- set vim to yank to system clipboard
 vim.opt.clipboard = 'unnamedplus'
