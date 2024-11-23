6a6dcfff01bf2aeb21af837242ebcd2b8b1a321f
FlyMandi <steidlmartinez@gmail.com>
2024-11-23 12:38:57 +0100

    finally remove typo

--- nvim/lua/plugins/tresitter.lua
+++
@@ -1,8 +0,0 @@
-return {
-    {
-        "nvim-treesitter/nvim-treesitter", build = "TSUpdate",
-    ensure_installed = {"c", "cpp", "markdown", "yaml", "toml", "vim", "vimdoc", "markdown_inline", "lua"},
-    highlight = { enable = true },
-    indent = { enable = true },
-    },
-}
