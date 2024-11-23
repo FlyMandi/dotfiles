a3136c902c8e41fca15d532fedbcd7feee41ed81
Mandi <115730623+FlyMandi@users.noreply.github.com>
2024-11-23 12:11:36 +0100

    Update tresitter.lua

--- nvim/lua/plugins/tresitter.lua
+++ nvim/lua/plugins/tresitter.lua
@@ -1,7 +1,7 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter", build = "TSUpdate",
-    ensure_installed = {"c", "cpp", "markdown", "yaml", "toml", "vim", "vimdoc", "markdown_inline"},
+    ensure_installed = {"c", "cpp", "markdown", "yaml", "toml", "vim", "vimdoc", "markdown_inline", "lua"},
     highlight = { enable = true },
     indent = { enable = true },
     },
