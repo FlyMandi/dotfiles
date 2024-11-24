37fe59ae9e8b5e1d844dcfc2abc27cbb3ab5305f
Federico <steidlmartinez@gmail.com>
2024-11-24 20:53:05 +0100

    set treesitter to autoinstall

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,9 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter", build = "TSUpdate",
-    ensure_installed = {"c", "cpp", "markdown", "yaml", "toml", "vim", "vimdoc", "markdown_inline", "lua"},
-    highlight = { enable = true },
-    indent = { enable = true },
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = "TSUpdate",
+        auto_install = true,
+		highlight = { enable = true },
+		indent = { enable = true },
+	},
 }
