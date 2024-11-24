7fc94a2fb3d96f8ca22831e54db01a9d065f6297
Federico <steidlmartinez@gmail.com>
2024-11-24 20:01:03 +0100

    clean up init

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,7 +12,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
       "clone",
       "--filter=blob:none",
       "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable", -- latest stable release
+      "--branch=stable",  
       lazypath,
     })
 end
@@ -21,6 +21,7 @@ vim.opt.rtp:prepend(lazypath)
 -- load all plugins in lua\plugins
 require("lazy").setup("plugins")
 
--- set colour scheme from lua\themes.lua:
--- tokyonight-night, toykonight-storm, tokyonight-moon, gruvbox
+-- vim.cmd[[colorscheme tokyonight-night]]
+-- vim.cmd[[colorscheme tokyonight-storm]]
+-- vim.cmd[[colorscheme tokyonight-moon]]
 vim.cmd[[colorscheme gruvbox]]
