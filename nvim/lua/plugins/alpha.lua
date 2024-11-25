65096b48cf5e5c89c1b175af32edf2a08c02d13e
Federico <steidlmartinez@gmail.com>
2024-11-25 21:11:10 +0100

    remove unecessary vars

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -4,10 +4,8 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.startify")
 
-		dashboard.section.header.val = {
+		require("alpha.themes.startify").section.header.val = {
 			[[                                                                       ]],
 			[[                                                                       ]],
 			[[                                                                       ]],
@@ -24,6 +22,6 @@ return {
 			[[                                                                       ]],
 			[[                                                                       ]],
 		}
-		alpha.setup(dashboard.opts)
+		require("alpha").setup(require("alpha.themes.startify").opts)
 	end,
 }
