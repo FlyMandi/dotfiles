b3635bf34172db6bdaca8622689af931a6e14ae3
Federico <steidlmartinez@gmail.com>
2024-11-25 21:17:28 +0100

    fix format

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,9 +2,9 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8",
-        dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()	
-            require("telescope").setup({
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
 				pickers = {
 					colorscheme = {
 						enable_preview = true,
@@ -22,24 +22,29 @@ return {
 					},
 				},
 			})
-            
-            require("telescope").load_extension("zoxide")
 
-            local builtin = require("telescope.builtin")
-            
-            vim.keymap.set("n", "<C-p>", "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<CR>", {} )
-            vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<cr>", {})
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
-            vim.keymap.set("n", "<leader>fz", ":Telescope zoxide list<CR>", {})
-            require("telescope").load_extension("ui-select")
+			require("telescope").load_extension("zoxide")
+
+			local builtin = require("telescope.builtin")
+
+			vim.keymap.set(
+				"n",
+				"<C-p>",
+				"<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<CR>",
+				{}
+			)
+			vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<cr>", {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
+			vim.keymap.set("n", "<leader>fz", ":Telescope zoxide list<CR>", {})
+			require("telescope").load_extension("ui-select")
 		end,
 	},
 
--- telescope addons
+	-- telescope addons
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
-        config = function()
+		config = function()
 			require("telescope").setup({
 				extensions = {
 					["ui-select"] = {
@@ -49,12 +54,10 @@ return {
 					},
 				},
 			})
-			-- To get ui-select loaded and working with telescope, you need to call
-			-- load_extension, somewhere after setup function:
 			require("telescope").load_extension("ui-select")
 		end,
 	},
-    {
-        "jvgrootveld/telescope-zoxide",
-    },
+	{
+		"jvgrootveld/telescope-zoxide",
+	},
 }
