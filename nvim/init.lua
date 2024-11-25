23a3f7394fa6e1d5db73e9546df1ae93f91873c2
Federico <steidlmartinez@gmail.com>
2024-11-25 21:12:00 +0100

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader=" "
+vim.g.mapleader = " "
 
 vim.g.have_nerd_font = true
 vim.opt.termguicolors = true
@@ -8,18 +8,15 @@ require("nvimoptions")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable",  
-      lazypath,
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 
--- vim.cmd[[colorscheme tokyonight-night]]
--- vim.cmd[[colorscheme tokyonight-storm]]
--- vim.cmd[[colorscheme tokyonight-moon]]
-vim.cmd[[colorscheme gruvbox]]
+vim.cmd([[colorscheme gruvbox]])
