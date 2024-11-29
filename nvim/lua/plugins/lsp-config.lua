8b4f9259919c89cec2962b336fb2709f5fad0c06
Federico <steidlmartinez@gmail.com>
2024-11-29 01:39:51 +0100

    remove glslls since it doesn't work

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -3,7 +3,9 @@ return {
         "williamboman/mason.nvim",
         lazy = false,
         config = function()
-            require("mason").setup()
+            require("mason").setup({
+                log_level = vim.log.levels.DEBUG
+            })
         end,
     },
     {
@@ -11,7 +13,7 @@ return {
         lazy = false,
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "clangd", "glslls" },
+                ensure_installed = { "lua_ls", "clangd", },
             })
         end,
     },
@@ -19,8 +21,13 @@ return {
         "neovim/nvim-lspconfig",
         lazy = false,
         config = function()
+
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
+
+            lspconfig.glsl_analyzer.setup({
+                capabilities = capabilities,
+            })
 
             lspconfig.lua_ls.setup({
                 capabilities = capabilities,
@@ -32,10 +39,6 @@ return {
             lspconfig.powershell_es.setup({
                 capabilities = capabilities,
                 bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services/",
-            })
-
-            lspconfig.glslls.setup({
-                capabilities = capabilities,
             })
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
