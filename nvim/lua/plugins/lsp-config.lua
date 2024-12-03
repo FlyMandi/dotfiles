a9a801294fed7903339cba4e199bd82072fbde27
FlyMandi <steidlmartinez@gmail.com>
2024-12-03 18:35:06 +0000

    ensure powershell_es installed

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -13,7 +13,7 @@ return {
         lazy = false,
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "clangd", },
+                ensure_installed = { "lua_ls", "clangd", "powershell_es"},
             })
         end,
     },
