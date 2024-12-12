a66a44c5f9e8673885bc80d213b32767d5a7a1db
FlyMandi <steidlmartinez@gmail.com>
2024-12-12 15:23:53 -0300

    comment out next error

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -16,8 +16,8 @@ return {
         require("todo-comments").jump_prev()
       end, { desc = "Previous todo comment" })
 
-      vim.keymap.set("n", "]t", function()
-        require("todo-comments").jump_next({keywords = { "ERROR", "WARNING" }})
-      end, { desc = "Next error/warning todo comment" })  
+      --vim.keymap.set("n", "]t", function()
+        --require("todo-comments").jump_next({keywords = { "ERROR", "WARNING" }})
+      --end, { desc = "Next error/warning todo comment" })  
     end
 }
