68fd7e0ce5cd7211ab3acb22b8ac002dcc498d29
FlyMandi <steidlmartinez@gmail.com>
2024-12-13 11:33:22 -0300

    fix TODO jumps

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -8,16 +8,30 @@ return {
     },
     config = function()
         require("todo-comments").setup()
-      vim.keymap.set("n", "]t", function()
-        require("todo-comments").jump_next()
-      end, { desc = "Next todo comment" })
+            vim.keymap.set("n", "]t", function()
+                require("todo-comments").jump_next()
+            end, { desc = "Next marked comment" })
 
-      vim.keymap.set("n", "[t", function()
-        require("todo-comments").jump_prev()
-      end, { desc = "Previous todo comment" })
+            vim.keymap.set("n", "[t", function()
+                require("todo-comments").jump_prev()
+            end, { desc = "Previous marked comment" })
 
-      --vim.keymap.set("n", "]t", function()
-        --require("todo-comments").jump_next({keywords = { "ERROR", "WARNING" }})
-      --end, { desc = "Next error/warning todo comment" })  
+
+            vim.keymap.set("n", "t]", function()
+                require("todo-comments").jump_next({keywords = {"TODO",}})
+            end, { desc = "Next todo comment" })
+
+            vim.keymap.set("n", "t[", function()
+                require("todo-comments").jump_prev({keywords = {"TODO",}})
+            end, { desc = "Previous todo comment" })
+
+
+            vim.keymap.set("n", "]]t", function()
+                require("todo-comments").jump_next({keywords = { "ERROR", "WARNING", "BUG", "FIXME",}})
+            end, { desc = "Next error/warning todo comment" })
+
+            vim.keymap.set("n", "[[t", function()
+                require("todo-comments").jump_prev({keywords = { "ERROR", "WARNING", "BUG", "FIXME",}})
+            end, { desc = "Previous error/warning todo comment" })
     end
 }
