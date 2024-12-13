9d2e990b2da41478b6b1a7e38c7ccf0838404006
FlyMandi <steidlmartinez@gmail.com>
2024-12-13 14:42:40 -0300

    C:\Repository\ -> %Repo%

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,7 @@
 Import-Module gsudoModule
 
 Set-PSReadlineOption -PredictionSource None
-if(Test-Path "T:\Repository\"){
-    $repo = "T:\Repository\"
-}
-elseIf(Test-Path "C:\Repository\"){
-    $repo = "C:\Repository\"
-}
+
 New-Alias Goto Set-Location
 New-Alias fetch winfetchconfig
 New-Alias vim nvim
@@ -25,5 +20,5 @@ function Get-Graph {
     Invoke-RestMethod "wttr.in/$city`?format=v2"
 }
 
-oh-my-posh init pwsh --config (Join-Path -PATH $repo -ChildPath "\dotfiles\PowerShell\Mandi.omp.json") | Invoke-Expression
+oh-my-posh init pwsh --config (Join-Path -PATH $env:Repo -ChildPath "\dotfiles\PowerShell\Mandi.omp.json") | Invoke-Expression
 
