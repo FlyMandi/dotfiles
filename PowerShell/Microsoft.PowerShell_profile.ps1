44ff9091d2b0edfdff1e601b2cb0b28e6a893695
FlyMandi <steidlmartinez@gmail.com>
2024-12-20 14:44:24 -0300

    refactor

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,20 +5,23 @@ Set-PSReadlineOption -PredictionSource None
 New-Alias Goto Set-Location
 New-Alias fetch winfetchconfig
 New-Alias csh cheatsheet
+New-Alias gw Get-Weather
 New-Alias vim nvim
 
-function Get-Weather {
+$toRemove = "Follow*"
+function Get-Weather{
     param(
-        $city = ""
+        $city = "",
+        [switch]$graph = $false
     )
-    Invoke-RestMethod "wttr.in/$city"
-}
-
-function Get-Graph {
-    param(
-        $city = ""
-    )
-    Invoke-RestMethod "wttr.in/$city`?format=v2"
+    
+    if ( $graph ){ $output = Invoke-RestMethod "wttr.in/$city`?format=v2" | Out-String }
+    else { $output = Invoke-RestMethod "wttr.in/$city" | Out-String }
+    $outputLines = $output -split "`n"
+    $filteredLines = $outputLines | Where-Object {$_ -notmatch $toRemove}
+    $cleanedlines = $filteredLines | Where-Object {$_.trim() -ne ""}
+    $cleanedlines -join "`n"
+    Write-Host ""
 }
 
 oh-my-posh init pwsh --config (Join-Path -PATH $env:Repo -ChildPath "\dotfiles\PowerShell\Mandi.omp.json") | Invoke-Expression
