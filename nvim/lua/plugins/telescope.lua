3d014b6431b4c69ca096e3741f07fe392e99c37e
Federico <steidlmartinez@gmail.com>
2025-03-06 22:31:38 +0100

    add emoji.nvim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -43,6 +43,17 @@ return {
 
 	-- telescope addons
 	{
+		"allaman/emoji.nvim",
+		ft = "markdown",
+		config = function()
+			require("emoji").setup({
+				enable_cmp_integration = true,
+			})
+            local ts = require("telescope").load_extension("emoji")
+			vim.keymap.set("n", "<leader>se", ts.emoji, { desc = "[S]earch [Emoji]" })
+		end,
+	},
+	{
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
 			require("telescope").setup({
