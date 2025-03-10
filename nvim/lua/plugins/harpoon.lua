a93f4ccf11602a03fc194e1fcb9d57f5020e6112
Federico <steidlmartinez@gmail.com>
2025-03-10 10:35:58 +0100

    change branch to harpoon2

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,6 +1,7 @@
 return {
     {
         "ThePrimeagen/harpoon",
+        branch = "harpoon2",
         config = function()
             HarpoonUI = require("harpoon.ui")
             local harpoon = require("harpoon")
