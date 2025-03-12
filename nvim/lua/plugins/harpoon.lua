4aadfb388fb071b7d3089930acce15c8eccf0e01
Federico <steidlmartinez@gmail.com>
2025-03-12 23:40:27 +0100

    fix

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -2,19 +2,20 @@ return {
     {
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        lazy = false,
         config = function()
-            require("harpoon"):setup()
-            vim.keymap.set("n", "<Esc>", "<ESC>")
+            local harpoon = require("harpoon")
+            harpoon:setup()
+
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<C-k>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+            vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+            vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<C-e>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<C-i>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<C-o>", function() harpoon:list():select(4) end)
         end,
-        keys = {
-            { "<leader>a", function() require("harpoon.mark").add_file() end, desc = "harpoon file", },
-            { "<C-k>", function() require("harpoon.ui").toggle_quick_menu() end, desc = "harpoon quick menu", },
-            { "<C-]>", function() require("harpoon.ui").nav_next() end, desc = "harpoon to file 4", },
-            { "<C-[>", function() require("harpoon.ui").nav_prev() end, desc = "harpoon to file 4", },
-            { "<C-n>", function() require("harpoon.ui").nav_file(1) end, desc = "harpoon to file 1", },
-            { "<C-e>", function() require("harpoon.ui").nav_file(2) end, desc = "harpoon to file 2", },
-            { "<C-i>", function() require("harpoon.ui").nav_file(3) end, desc = "harpoon to file 3", },
-            { "<C-o>", function() require("harpoon.ui").nav_file(4) end, desc = "harpoon to file 4", },
-        },
     }
 }
