ca454d40732eb59e7f3857e9d3ca4285d94ab7a3
Federico <steidlmartinez@gmail.com>
2025-03-12 22:59:34 +0100

    fix format & font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,13 @@
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 local config = {}
 
 config.front_end = "OpenGL"
 config.term = "xterm-256color"
 config.prefer_egl = true
-config.font = wezterm.font 'Cascadia Code NF'
-config.color_scheme = 'Dark Pastel'
-config.window_background_opacity = 0.9
-config.max_fps = 165
-config.font_size = 14.5
+config.font = wezterm.font("Cascadia Code NF")
+config.color_scheme = "Dark Pastel"
+config.window_background_opacity = 0.75
+config.font_size = 14
 config.default_cursor_style = "BlinkingBlock"
 config.animation_fps = 1
 config.cursor_blink_rate = 500
