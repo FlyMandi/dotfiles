c7a170c170bf9a3a266e4cf1fd5b529bc5af044e
Federico <steidlmartinez@gmail.com>
2025-03-17 14:01:50 +0100

    fix <ESC> w/ harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -8,6 +8,7 @@ return {
             local harpoon = require("harpoon")
             harpoon:setup()
 
+            vim.keymap.set("n", "<ESC>", "<ESC>")
             vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
             vim.keymap.set("n", "<C-k>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
             vim.keymap.set("n", "<C-]>", function() harpoon:list():next() end)
