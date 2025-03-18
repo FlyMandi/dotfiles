dbd02e9445e5134a1b295b63ba68e2418c5be251
Federico <steidlmartinez@gmail.com>
2025-03-18 05:35:26 +0100

    update treesitter context

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,21 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-        auto_install = true,
+		auto_install = true,
 		highlight = { enable = true },
-        ensure_installed = {"cpp", "lua"},
+		ensure_installed = { "cpp", "lua" },
 		indent = { enable = true },
 	},
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-        config = function()
-            require("treesitter-context").setup({
-                enable = true,
-                line_numbers = true,
-            })
-        end,
-    },
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		-- config = function()
+		--     require("treesitter-context").setup({
+		--         enable = true,
+		--         line_numbers = true,
+		--     })
+		-- end,
+		vim.keymap.set("n", "[c", function()
+			require("treesitter-context").go_to_context(vim.v.count1)
+		end, { silent = true }),
+	},
 }
