99c31bbf4c3d726ff8d31d071ba8c05c356e467d
Federico <steidlmartinez@gmail.com>
2025-03-23 13:09:47 +0100

    opaqueness++

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,12 +6,9 @@ config.term = "xterm-256color"
 config.prefer_egl = true
 config.font = wezterm.font("Cascadia Code NF")
 config.color_scheme = "Dark Pastel"
-config.window_background_opacity = 0.5
+config.window_background_opacity = 0.75
 config.font_size = 14
 config.default_cursor_style = "SteadyBlock"
--- config.force_reverse_video_cursor = true
--- config.animation_fps = 1
--- config.cursor_blink_rate = 500
 
 config.default_prog = { "pwsh.exe" }
 
