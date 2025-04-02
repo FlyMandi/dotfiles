8f57f0a5bef7668889444db8225612dc881c3374
Federico <steidlmartinez@gmail.com>
2025-04-02 16:23:26 +0200

    remap so there are no conflicts

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -7,17 +7,14 @@ return {
 		keys = {
 			{ "<leader>a", "<cmd>lua require('harpoon'):list():add()<CR>" },
 			{ "<C-k>", "<cmd>lua require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())<CR>" },
-			{ "<C-]>", "<cmd>lua require('harpoon'):list():next()<CR>" },
-			{ "<C-[>", "<cmd>lua require('harpoon'):list():prev()<CR>" },
-			{ "<C-n>", "<cmd>lua require('harpoon'):list():select(1)<CR>" },
-			{ "<C-e>", "<cmd>lua require('harpoon'):list():select(2)<CR>" },
-			{ "<C-i>", "<cmd>lua require('harpoon'):list():select(3)<CR>" },
-			{ "<C-o>", "<cmd>lua require('harpoon'):list():select(4)<CR>" },
+			{ "<C-l>", "<cmd>lua require('harpoon'):list():select(1)<CR>" },
+			{ "<C-n>", "<cmd>lua require('harpoon'):list():select(2)<CR>" },
+			{ "<C-e>", "<cmd>lua require('harpoon'):list():select(3)<CR>" },
+			{ "<C-u>", "<cmd>lua require('harpoon'):list():select(4)<CR>" },
 		},
 		config = function()
 			local harpoon = require("harpoon")
 			harpoon:setup()
 		end,
-		vim.keymap.set("n", "<ESC>", "<ESC>"),
 	},
 }
