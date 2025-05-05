7d644a56d49e31d3ca57928bbbad826f29c4e26a
Federico <steidlmartinez@gmail.com>
2025-05-05 08:20:13 +0200

    darken transparency

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,7 @@ config.term = "xterm-256color"
 config.prefer_egl = true
 config.font = wezterm.font("Cascadia Code NF")
 config.color_scheme = "Dark Pastel"
-config.window_background_opacity = 0.5
+config.window_background_opacity = 0.8
 config.font_size = 14
 config.default_cursor_style = "SteadyBlock"
 
