199fc65c53ae5e1c6c9677e7b1b7efb8e8253407
Federico <steidlmartinez@gmail.com>
2025-05-11 14:53:24 +0200

    update, add jdtls

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -1,7 +1,13 @@
-return{
+return {
 	"mfussenegger/nvim-dap",
 	lazy = true,
 	dependencies = {
+		"Willem-J-an/nvim-dap-powershell",
+		{
+			"m00qek/baleia.nvim",
+			lazy = true,
+			tag = "v1.4.0",
+		},
 		"rcarriga/nvim-dap-ui",
 		"theHamsta/nvim-dap-virtual-text",
 		"nvim-neotest/nvim-nio",
@@ -12,6 +18,49 @@ return{
 		local ui = require("dapui")
 
 		require("dapui").setup()
+		require("dap-powershell").setup()
+
+		dap.adapters.gdb = {
+			type = "executable",
+			command = "gdb",
+			args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+		}
+
+		dap.configurations.c = {
+			{
+				name = "Launch",
+				type = "gdb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopAtBeginningOfMainSubprogram = false,
+			},
+			{
+				name = "Select and attach to process",
+				type = "gdb",
+				request = "attach",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				pid = function()
+					local name = vim.fn.input("Executable name (filter): ")
+					return require("dap.utils").pick_process({ filter = name })
+				end,
+				cwd = "${workspaceFolder}",
+			},
+			{
+				name = "Attach to gdbserver :1234",
+				type = "gdb",
+				request = "attach",
+				target = "localhost:1234",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+			},
+		}
 
 		dap.listeners.before.attach.dapui_config = function()
 			ui.open()
@@ -24,6 +73,12 @@ return{
 		end
 		dap.listeners.before.event_exited.dapui_config = function()
 			ui.close()
+		end
+
+		--fix pwsh error messages
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			ui.open()
+			require("dap-powershell").correct_repl_colors()
 		end
 	end,
 	-- keys = {
