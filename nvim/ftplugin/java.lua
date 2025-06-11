2c247ac55bea3c074407a11f195fae898469ea3a
Federico <steidlmartinez@gmail.com>
2025-06-11 18:16:45 +0200

    fix

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,5 +1,17 @@
 local config = {
-    cmd = {vim.fn.stdpath("data") .. "/mason/bin/jdtls"},
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw', 'pom.xml'}, { upward = true })[1]),
+	cmd = { vim.fn.stdpath("data") .. "/mason/bin/jdtls" },
+	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw", "pom.xml" }, { upward = true })[1]),
+	init_options = {
+		bundles = {
+			vim.fn.glob(
+				(
+					-- vim.fn.stdpath("data")
+					-- .. "/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"
+					"C:/Users/steidl/Appdata/local/nvim/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"
+				),
+				1
+			),
+		},
+	},
 }
 require("jdtls").start_or_attach(config)
