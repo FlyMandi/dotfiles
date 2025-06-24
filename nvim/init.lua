12766cde04f2b24b2a3ce6dbb7ea60dad5440046
Federico <steidlmartinez@gmail.com>
2025-06-24 13:37:19 +0200

    add autocmd to remove empty lines & trailing whitespace

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,16 +7,25 @@ require("nvimoptions")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 
 vim.cmd([[colorscheme gruvbox]])
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function()
+		vim.cmd([[%s/\_s*\%$//e]])
+		vim.cmd([[%s/\s\+$//e]])
+		vim.cmd([[%s/\n\{3,}/\r\r/e]])
+	end,
+})
