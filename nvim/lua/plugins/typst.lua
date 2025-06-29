08f58795da212e21d1c1d2a1f7dfbb469d87666f
Federico <steidlmartinez@gmail.com>
2025-06-29 22:48:10 +0200

    fix typo

--- nvim/lua/plugins/typst.lua
+++ nvim/lua/plugins/typst.lua
@@ -4,12 +4,12 @@ return {
 		lazy = true,
 		ft = "typst",
 		version = "1.*",
-		config = function()
-			require("typst-preview").setup()({
-				dependencies_bin = {
-					["tinymist"] = "tinymist",
-				},
-			})
-		end,
+        config = function()
+            require("typst-preview").setup({
+                dependencies_bin = {
+                    ["tinymist"] = "tinymist",
+                },
+            })
+        end,
 	},
 }
