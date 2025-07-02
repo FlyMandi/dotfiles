6d10bc699b4292a10480364dc01907a83419c5dc
Federico <steidlmartinez@gmail.com>
2025-07-02 20:41:34 +0200

    greatly update colourschemes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,14 +5,94 @@ config.front_end = "OpenGL"
 config.term = "xterm-256color"
 config.prefer_egl = true
 config.font = wezterm.font("Cascadia Code NF")
-config.color_scheme = "Dark Pastel"
-config.window_background_opacity = 0.8
-config.font_size = 14
+config.window_background_opacity = 0.85
+config.font_size = 13.75
 config.default_cursor_style = "SteadyBlock"
 
 config.default_prog = { "pwsh.exe" }
 
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
+
+config.colors = {
+  foreground = '#DDAA50',
+  background = '#000000',
+
+  -- Overrides the cell background color when the current cell is occupied by the
+  -- cursor and the cursor style is set to Block
+  cursor_bg = '#DDAA50',
+  -- Overrides the text color when the current cell is occupied by the cursor
+  cursor_fg = '#DDFFFF',
+  -- Specifies the border color of the cursor when the cursor style is set to Block,
+  -- or the color of the vertical or horizontal bar when the cursor style is set to
+  -- Bar or Underline.
+  cursor_border = '#DDAA50',
+
+  -- the foreground color of selected text
+  selection_fg = '#000000',
+  -- the background color of selected text
+  selection_bg = '#5025FF',
+
+  -- The color of the scrollbar "thumb"; the portion that represents the current viewport
+  scrollbar_thumb = '#222222',
+
+  -- The color of the split lines between panes
+  split = '#444444',
+
+  ansi = {
+    '#000000',
+    '#EE2020',
+    '#00FF00',
+    '#DDAA50',
+    '#0000FF',
+    '#FF00FF',
+    '#00FFFF',
+    '#AAAAAA',
+  },
+  brights = {
+    '#DCDCDC',
+    '#EE2020',
+    '#00FF00',
+    '#DDAA50',
+    '#0000FF',
+    '#FF00FF',
+    '#00FFFF',
+    '#FFFFFF',
+  },
+
+  -- Arbitrary colors of the palette in the range from 16 to 255
+  -- indexed = { [136] = '#af8700' },
+
+  -- Since: 20220319-142410-0fcdea07
+  -- When the IME, a dead key or a leader key are being processed and are effectively
+  -- holding input pending the result of input composition, change the cursor
+  -- to this color to give a visual cue about the compose state.
+  compose_cursor = 'orange',
+
+  -- Colors for copy_mode and quick_select
+  -- available since: 20220807-113146-c2fee766
+  -- In copy_mode, the color of the active text is:
+  -- 1. copy_mode_active_highlight_* if additional text was selected using the mouse
+  -- 2. selection_* otherwise
+  copy_mode_active_highlight_bg = { Color = '#000000' },
+  -- use `AnsiColor` to specify one of the ansi color palette values
+  -- (index 0-15) using one of the names "Black", "Maroon", "Green",
+  --  "Olive", "Navy", "Purple", "Teal", "Silver", "Grey", "Red", "Lime",
+  -- "Yellow", "Blue", "Fuchsia", "Aqua" or "White".
+  copy_mode_active_highlight_fg = { AnsiColor = 'Black' },
+  copy_mode_inactive_highlight_bg = { Color = '#52ad70' },
+  copy_mode_inactive_highlight_fg = { AnsiColor = 'White' },
+
+  quick_select_label_bg = { Color = 'peru' },
+  quick_select_label_fg = { Color = '#ffffff' },
+  quick_select_match_bg = { AnsiColor = 'Navy' },
+  quick_select_match_fg = { Color = '#ffffff' },
+
+  input_selector_label_bg = { AnsiColor = 'Black' }, -- (*Since: Nightly Builds Only*)
+  input_selector_label_fg = { Color = '#ffffff' }, -- (*Since: Nightly Builds Only*)
+
+  launcher_label_bg = { AnsiColor = 'Black' }, -- (*Since: Nightly Builds Only*)
+  launcher_label_fg = { Color = '#ffffff' }, -- (*Since: Nightly Builds Only*)
+}
 
 return config
