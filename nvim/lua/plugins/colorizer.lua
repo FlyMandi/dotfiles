349e6e8336917fca6bfc75f7154542a40604d175
Federico <steidlmartinez@gmail.com>
2025-07-02 20:08:09 +0200

    fix colours not showing

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,8 +1,12 @@
 return {
 	"norcalli/nvim-colorizer.lua",
 	lazy = true,
-	event = "LspAttach",
+	event = {"BufReadPre", "BufNewFile"},
 	config = function()
-		require("colorizer").setup({ "*", css = { rgb_fn = true } })
+		require("colorizer").setup({
+			"*",
+            css = { rgb_fn = true },
+		})
 	end,
+    mode = "foreground"
 }
