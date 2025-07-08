6406ff90cace5889537ea3a25de66c11f8d7a143
Federico <steidlmartinez@gmail.com>
2025-07-09 00:24:53 +0200

    move back pk to lazy keys

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -3,11 +3,11 @@ return {
     keys = {
         { "go", "<cmd>Lspsaga goto_definition<CR>",         mode = "n", noremap = true, silent = true },
         { "gh", "<cmd>Lspsaga finder<CR>",                  mode = "n", noremap = true, silent = true },
+        { "<leader>pk", "<cmd>Lspsaga peek_definition<CR>", mode = "n", noremap = true, silent = true },
         { "<leader>ci", "<cmd>Lspsaga incoming_calls<CR>",  mode = "n", noremap = true, silent = true },
         { "<leader>co", "<cmd>Lspsaga outgoing_calls<CR>",  mode = "n", noremap = true, silent = true },
     },
     config = function()
         require("lspsaga").setup()
-    vim.keymap.set('n', '<leader>pk', '<cmd>Lspsaga peek_definition<CR>', { silent = true, noremap = true })
     end
 }
