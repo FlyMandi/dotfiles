f4fe090116b6c118623e5e482d4eb3bdd61ef04e
--global <steidlmartinez@gmail.com>
2025-11-18 16:30:45 +0100

    update config

--- nvim/lua/nvimoptions.lua
+++ nvim/lua/nvimoptions.lua
@@ -2,12 +2,13 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 vim.wo.wrap = false
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
 vim.opt.clipboard = "unnamedplus"
+
 vim.opt.guicursor = "n-v-c-i:block-Cursor/lcursor"
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "line"
