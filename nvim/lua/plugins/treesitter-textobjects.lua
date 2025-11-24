b50eca947c3c06da95b812b22af1b50e63313c26
FlyMandi <steidlmartinez@gmail.com>
2025-11-24 09:25:35 +0100

    update textobject keybinds

--- nvim/lua/plugins/treesitter-textobjects.lua
+++ nvim/lua/plugins/treesitter-textobjects.lua
@@ -8,10 +8,13 @@ return {
                   enable = true,
                   set_jumps = true,
                   goto_next_start = {
-                      ["[f"] = "@function.outer",
+                      ["]f"] = "@function.outer",
+                  },
+                  goto_next_end = {
+                      ["]F"] = "@function.outer",
                   },
                   goto_previous_start = {
-                      ["]f"] = "@function.outer",
+                      ["[f"] = "@function.outer",
                   },
                   goto_previous_end = {
                       ["[F"] = "@function.outer",
